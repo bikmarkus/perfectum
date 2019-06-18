63001cadb6509f1ebbc56188ddf2d23f0f7c5c82
Linar Biktimirov <bikmarkus@gmail.com>
2019-06-18 14:05:10 +0400

    Set up list writing controller

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 	$("#sidebar").on("click","li a",function(){
-		console.log($(this).html());
 		$.post("listreader.php", {listname : $(this).html()}, function(data){
 			$("#main").html(data);
 		});
 		return false;
 	});
+    $("#main").on("click",".sortpanel button",function(){
+        var itemId = $(this).parent().parent().attr("data-num");
+        $.post("listwriter.php", {listname : $(this).html(), id : itemId}, function(data){
+            $("#main").html(data);
+        });
+        return false;
+    });
 });
