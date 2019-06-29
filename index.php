2c1e04b2c949be64616ff3bd7b4da3af4b8a6d54
Linar Biktimirov <bikmarkus@gmail.com>
2019-06-29 14:37:00 +0400

    Routing and list view added

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
 <?php
-	echo '<pre>';
+/*	echo '<pre>';
 	var_dump($_SERVER['REQUEST_URI']);
 	echo '</pre>';
+*/
+	error_reporting(E_ALL);
+	ini_set('display_errors',1);
+	
+	// Назначаем модуль и действие по умолчанию
+	$module = 'index';
+	$action = 'index';
+
+	// Массив параметров из URI запроса
+	$params = array();
+
+	// Если запрошен любой URL кроме корня сайта
+	if($_SERVER['REQUEST_URI'] != '/perfectum/')
+	{
+		$url_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		
+		// Разбиваем виртуальный URL по символу /
+		$uri_parts = explode('/', trim($url_path,'/'));
+			
+		// Убираем домен из url:
+		if($uri_parts[0]=='perfectum') array_shift($uri_parts);	
+
+		$module = array_shift($uri_parts); // Получили имя модуля
+		$action = array_shift($uri_parts); // Получили имя действия
+
+		// Получили в $params параметры запроса
+		for($i=0;$i<count($uri_parts);$i++)
+		{
+			$params[$uri_parts[$i]] = $uri_parts[++$i];
+		}
+	}
+	else 
+	{
+		include_once('list/view.php');
+		die();
+	}
+	echo "\$module: $module\n";
+	echo "\$action: $action\n";
+	echo "\$params:\n";
+	print_r($params);
 ?>
+
---
+++ list/view.php
@@ -0,0 +1,43 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Perfectum</title>
+	<link rel="stylesheet" href="assets/styles.css" />
+	<script src="assets/jquery.js"></script>
+	<script src="assets/script.js"></script>	
+</head>
+
+<body>
+	<main>			
+		<div class="innertube">
+			<h1>Inbox</h1>
+		</div>
+		<div class="item-container">
+			<ul class="item-list">
+				<li class="item"><div class="item-row"><span>Task 1</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 2</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 3</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 4</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 5</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 6</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 7</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 8</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 9</span></div></li>
+				<li class="item"><div class="item-row"><span>Task 10</span></div></li>
+			</ul>
+		</div>
+	</main>
+	<nav id="nav">
+		<div class="innertube">	
+			<h3>Control panel</h3>
+			<ul>
+				<li><a href="#">Inbox</a></li>
+				<li><a href="#">Next</a></li>
+				<li><a href="#">Projects</a></li>
+				<li><a href="#">Someday</a></li>
+				<li><a href="#">Reference</a></li>
+			</ul>
+		</div>
+	</nav>
+</body>
+</html> 
